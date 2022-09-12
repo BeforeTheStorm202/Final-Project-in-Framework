5974467a31a6d61747ff6aae79eb380792d31b63
BeforeTheStorm202 <haro20254@LORD-HARO.localdomain>
2022-09-12 15:58:38 +0700

    add server again

---
+++ Server/Server.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Server
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnListen = new System.Windows.Forms.Button();
+            this.view = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnListen
+            // 
+            this.btnListen.Location = new System.Drawing.Point(12, 21);
+            this.btnListen.Name = "btnListen";
+            this.btnListen.Size = new System.Drawing.Size(101, 49);
+            this.btnListen.TabIndex = 0;
+            this.btnListen.Text = "LISTEN";
+            this.btnListen.UseVisualStyleBackColor = true;
+            this.btnListen.Click += new System.EventHandler(this.btnListen_Click);
+            // 
+            // view
+            // 
+            this.view.Location = new System.Drawing.Point(12, 96);
+            this.view.Name = "view";
+            this.view.ReadOnly = true;
+            this.view.Size = new System.Drawing.Size(776, 342);
+            this.view.TabIndex = 1;
+            this.view.Text = "";
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.view);
+            this.Controls.Add(this.btnListen);
+            this.Name = "Server";
+            this.Text = "Server";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Server_FormClosed);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListen;
+        private System.Windows.Forms.RichTextBox view;
+    }
+}
+
